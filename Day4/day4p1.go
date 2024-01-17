895edc9c591b991efc2ee0ecc19d095a3925c309
Nazemrap <corentin.damas@gmail.com>
2024-01-17 12:02:05 +0100

    day 4 solution

---
+++ Day4/day4p1.go
@@ -0,0 +1,95 @@
+package day4
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strings"
+
+	"strconv"
+
+	"golang.org/x/exp/slices"
+)
+
+func Day4() int {
+
+	bytesText, err := os.ReadFile("./Day4/data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	FullText := string(bytesText)
+	result := analyze(FullText)
+
+	fmt.Println("Day 4 part 1 result is : ")
+	return result
+}
+
+func analyze(s string) int {
+
+	var points int
+	var accumulator int = 0
+
+	lines := strings.Split(s, "\n")
+
+	for _, line := range lines {
+		points = 0
+		winningNums, scratchedNums := lineToArr(line)
+
+		for _, scrNum := range winningNums {
+			if slices.Contains(scratchedNums, scrNum) {
+				points += 1
+			}
+		}
+
+		if points > 0 {
+			accumulator += int(math.Pow(2, float64(points-1)))
+		}
+
+	}
+	return accumulator
+}
+
+func lineToArr(line string) ([]int, []int) {
+	var winningNums string
+	var onGameNums string
+
+	midSplit := strings.Split(line, " | ")
+	winningNums = midSplit[0]
+	onGameNums = midSplit[1]
+	winningSplit := strings.Split(winningNums, ": ")
+	winningNums = winningSplit[1]
+
+	return strToArr(winningNums), strToArr(onGameNums)
+}
+
+func strToArr(s string) []int {
+
+	strArr := strings.Split(s, " ")
+	intArr := []int{}
+	for _, num := range strArr {
+		if num == " " || num == "" {
+			continue
+		}
+		intnum, err := strtoInt(num)
+		if err != nil {
+			continue
+		}
+		intArr = append(intArr, intnum)
+	}
+
+	return intArr
+}
+
+func strtoInt(s string) (int, error) {
+	s = strings.Trim(s, "\r")
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+	return i, nil
+}
+
+// should found 32001
--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 
-	day3part2 "github.com/Corentin-Damas/advent_code2023/Day3/part_2"
+	day4 "github.com/Corentin-Damas/advent_code2023/Day4"
 )
 
 func main() {
 	fmt.Println("Welcome to Corentin's advent of code 2023")
-	result := day3part2.Day3part2()
-	fmt.Printf("%v\n", result )
+	result := day4.Day4()
+	fmt.Printf("%v\n", result)
 }
