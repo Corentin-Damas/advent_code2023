5ad854f0b86d9e8875d1c786bb43def43d789d93
Nazemrap <corentin.damas@gmail.com>
2024-01-24 10:28:07 +0100

    Day 7 part 1 & 2 completed

--- Day7/day7p1.go
+++ Day7/day7p1.go
@@ -27,7 +27,7 @@ type PlayerData []Player
 func Day7p1() int {
 
 	// cardsMapValues := createCards()
-	bytesText, err := os.ReadFile("./Day7/datatest.txt")
+	bytesText, err := os.ReadFile("./Day7/data.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -36,7 +36,7 @@ func Day7p1() int {
 
 	result := analyzep1(puzzle)
 
-	fmt.Println("Day 7 part 1 result is : ")
+	fmt.Println("Day 7 part 2 result is : ")
 	return result
 }
 
@@ -45,18 +45,20 @@ func analyzep1(s string) int {
 
 	playerData := getPlayersDatas(lines)
 
-	orderPlayers := getRank(playerData)
+	pPlayerData := &playerData
 
-	for _, hand := range orderPlayers {
-		fmt.Printf("hand: %s bid:%d type: %s handpoint: %d rank %d \n", hand.hand, hand.bid, hand.handType, hand.handPoint, hand.rank)
-	}
+	acc := getRank(pPlayerData)
 
-	return 0
+	// for _, hand := range playerData {
+	// 	fmt.Printf("hand: %s bid:%d type: %s handpoint: %d rank %d \n", hand.hand, hand.bid, hand.handType, hand.handPoint, hand.rank)
+	// }
+
+	return acc
 
 }
 
 func singleCardValues() map[string]int {
-	labels := []string{"A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"}
+	labels := []string{"A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"}
 
 	cardValues := make(map[string]int)
 
@@ -86,9 +88,14 @@ func getPlayersDatas(lines []string) PlayerData {
 
 func scanHand(hand string) (string, int) {
 	cards := make(map[rune]int)
+	num_Joker := 0
 	var similarCard []int
+
 	for _, c := range hand {
 		cards[c] += 1
+		if string(c) == "J"{
+			num_Joker +=1
+		}
 	}
 	for _, val := range cards {
 		similarCard = append(similarCard, val)
@@ -98,41 +105,71 @@ func scanHand(hand string) (string, int) {
 		return "Five of a kind", 7
 	}
 	if slices.Contains(similarCard, 4) {
+
+		if num_Joker == 1 || num_Joker == 4{
+			return "Five of a kind", 7
+		}
 		return "Four of a kind", 6
 	}
 	if slices.Contains(similarCard, 3) && slices.Contains(similarCard, 2) {
+		if num_Joker == 2 || num_Joker == 3{
+			return "Five of a kind", 7
+		}
 		return "Full house", 5
 	}
 	if slices.Contains(similarCard, 3) {
+		if num_Joker == 1 || num_Joker == 3{
+			return "Four of a kind", 6
+		}
 		return "Three of a kind", 4
 	}
 	if slices.Contains(similarCard, 2) && len(similarCard) == 3 {
+		if num_Joker == 1{
+			return "Full house", 5
+		}
+		if num_Joker == 2{
+			return "Four of a kind", 6
+		}
+
 		return "Two pair", 3
 	}
 	if slices.Contains(similarCard, 2) && len(similarCard) == 4 {
+		if num_Joker == 1 || num_Joker == 2{
+			return "Three of a kind", 4
+		}
+
+		if num_Joker == 2{
+			return "Four of a kind", 6
+		}
+		return "One pair", 2
+	}
+
+	if num_Joker == 1 {
 		return "One pair", 2
 	}
 	return "High card", 1
 }
 
-func getRank(players PlayerData) []Player {
+func getRank(players *PlayerData) int {
+	accumulator := 0
 
 	results := make(map[int][]Player)
-	maxRank := len(players)
+	maxRank := len(*players)
 	fmt.Println("max Rank: ", maxRank)
 
-	var rank int = 1
+	var currRank int = 1
 
 	//Map each player by the Type of hand he had ( and the point it gave)
-	for _, hand := range players {
-		results[hand.handPoint] = append(results[hand.handPoint], hand)
+	for _, player := range *players {
+		results[player.handPoint] = append(results[player.handPoint], player)
 	}
 
 	// We will compare hand by hand 1 point = High card , 2 point = One pair ... 7 point = Five of a kind
 	for point := 1; point <= 7; point++ {
+		// fmt.Println("== Checking for Hand point = ", point)
 
 		// Break when all players has been ranked
-		if rank > maxRank {
+		if currRank > maxRank {
 			break
 		}
 
@@ -143,30 +180,47 @@ func getRank(players PlayerData) []Player {
 		}
 
 		// fmt.Printf("Looking at: %d , value: %v \n", point , arrPLayers)
-
 		// The player is alone to have a type of card -> we can give him a rank and pass to the next Hand Type
 		if len(arrPLayers) == 1 {
-			arrPLayers[0].rank = rank
-			rank += 1
-			// fmt.Printf("Looking at: %d , value: %v, rank %d \n", point , arrPLayers, arrPLayers[0].rank)
+			for _, pl := range *players {
+				if pl.hand == arrPLayers[0].hand {
+					pl.rank = currRank
+					// fmt.Printf("Looking at: %s rank %d \n", pl.hand, pl.rank)
+					accumulator += (pl.rank*pl.bid)
+					break
+				}
+			}
+			currRank += 1
 			continue
 		}
 
-		fmt.Printf("Looking at: %d , value: %v \n", point, arrPLayers)
+		// fmt.Printf("Array of players at: %d , value: %v \n", point, arrPLayers)
 		// We need to compare each player hands card by card
-
-		pArrPLayers := &arrPLayers
-		err := orderPlayerPerCardValue(pArrPLayers, 0)
+		var orderedPlayerArray []Player
+		pOrderedPlayerArray := &orderedPlayerArray
+		
+		err := orderPlayerPerCardValue(arrPLayers, pOrderedPlayerArray, 0)
 		if err != nil {
 			fmt.Errorf("we got an error in the ranking per card")
 		}
-
-		for _, player := range arrPLayers {
-			player.rank = rank
-			rank += 1
+		for i, j := 0, len(orderedPlayerArray)-1; i < j; i, j = i+1, j-1 {
+			orderedPlayerArray[i], orderedPlayerArray[j] = orderedPlayerArray[j], orderedPlayerArray[i]
+		}
+		
+		for _, player := range orderedPlayerArray {
+			// fmt.Println("Player from arrPLayers ", player)
+			for _, pl := range *players {
+				if pl.hand == player.hand {
+					pl.rank = currRank
+					accumulator += (pl.rank*pl.bid)
+					break
+				}
+			}
+			currRank += 1
 		}
 	}
-	return players
+
+	return accumulator
 
 }
 
@@ -180,30 +234,31 @@ func getArrValuePerCard(strCard string) []int {
 }
 
 // orderPlayerPerCardValue Take an array of Player and will sort them depending of there cards labels, considere that many players can get involved
-func orderPlayerPerCardValue(orderArr *[]Player, start int) error {
+func orderPlayerPerCardValue(notOrderPlayerArr []Player, inOrderPlayerArr *[]Player,start int) error {
 
-	mapArr := make(map[int][]Player)
+	mapPerCard := make(map[int][]Player)
 
 	if start > 5 {
 		err := errors.New("to much recursion")
 		return err
 	}
 
-	for _, player := range *orderArr {
-		mapArr[player.handValuePerCard[start]] = append(mapArr[player.handValuePerCard[start]], player)
-
+	for _, player := range notOrderPlayerArr {
+		mapPerCard[player.handValuePerCard[start]] = append(mapPerCard[player.handValuePerCard[start]], player)
 	}
-	fmt.Println(mapArr)
+	// fmt.Println("Ordering : ", mapPerCard)
 
 	for i := 15; i > 0; i-- {
-		arr, ok := mapArr[i]
+
+		arr, ok := mapPerCard[i]
 		if !ok {
 			continue
 		}
+
 		if len(arr) == 1 {
-			*orderArr = append(*orderArr, mapArr[i][0])
+			*inOrderPlayerArr= append(*inOrderPlayerArr, mapPerCard[i][0])
 		} else {
-			err := orderPlayerPerCardValue(orderArr, start+1)
+			err := orderPlayerPerCardValue(arr, inOrderPlayerArr,start+1)
 			if err != nil {
 				return err
 			}
@@ -222,3 +277,5 @@ func strtoInt(s string) int {
 	}
 	return i
 }
+
+
